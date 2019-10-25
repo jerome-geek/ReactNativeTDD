095e7f8fa5bab4a87687224513d101ff16f5fd87
Jerome <jerome@spinprotocol.io>
2019-10-25 09:21:14 +0900

    Install & Test Jest Framework

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -9,6 +9,9 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('Jest', () => {
+  it('is it working', () => {
+    const a = 1;
+    expect(a + 1).toBe(2);
+  });
 });
